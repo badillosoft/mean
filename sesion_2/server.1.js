553d1fb7462e3c4b709a9d73f921d9d919c3655e
Alan Badillo <badillo.soft@hotmail.com>
2018-05-16 14:34:31 -0500

    sesión 3

--- sesion_2/server.1.js
+++ sesion_2/server.1.js
@@ -8,9 +8,39 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// TODO: Agregar esquemas
+// -- ESQUEMAS Y MODELOS
+const ClientSchema = mongoose.Schema({
+    _id: String,
+    nombre: String,
+    correo: String,
+    contraseña: String
+});
+const Cliente = mongoose.model("Cliente", ClientSchema);
+// -- ESQUEMAS Y MODELOS
 
-// TODO: Agregar rutas
+// -- RUTAS
+app.put("/api/cliente/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, correo, contraseña, edad } = req.body;
+
+    const cliente = new Cliente({
+        _id: id,
+        correo: correo,
+        contraseña: contraseña,
+        nombre: nombre,
+        edad,
+    });
+
+    cliente.save().then(() => {
+        res.send({
+            mensaje: "Cliente guardado",
+            id
+        });
+    }).catch(err => {
+        res.status(400).send(err);
+    });
+});
+// -- RUTAS
 
 mongoose.connect("mongodb://localhost/mean").then(() => {
     http.createServer(app).listen(3000, () => {
